7467c10d828cdaca0dbbc209b9f7eaaab0eabbb7
Eduar1981 <edcorgriz@hotmail.com>
2023-11-24 19:00:01 -0500

    cambie la conexion de estar en la nuve a local

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
-MYSQL_HOST = 'sql10.freemysqlhosting.net'
+""" MYSQL_HOST = 'sql10.freemysqlhosting.net'
 MYSQL_USER = 'sql10661180'
 MYSQL_PASSWORD = 'E57UVmsWzF'
 MYSQL_DB = 'sql10661180'
+HEX_SEC_KEY = 'd5fb8c4fa8bd46638dadc4e751e0d68d'  """
+
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'eduar'
+MYSQL_PASSWORD = ''
+MYSQL_DB = 'mis_tareas'
 HEX_SEC_KEY = 'd5fb8c4fa8bd46638dadc4e751e0d68d' 
